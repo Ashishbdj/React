ef3054ced74f38e441a5e7b8794614d67f66e3a5
Ashish Kumar <kumarashish108108@gmail.com>
2025-09-24 13:45:49 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import JavascriptHTML from "./components/javascriptHTML"
 import ShopingCard from "./components/ShopingCArd"
+import img1 from "./assets/-original-imah4jyfveygyubc.webp"
+import img2 from "./assets/-original-imahfw4ahckefvxe.webp"
+import img3 from "./assets/-original-imahadxg8mgjzrfh.webp"
+import img4 from "./assets/nord-5-5g-cph2079-oneplus-original-imahdvhguqneybrf.webp"  
 function App()
 {
  
@@ -8,10 +12,10 @@ function App()
     <JavascriptHTML name="Ashish" age="22"/>
     <JavascriptHTML name="Anuj" age="20"/>
     <div style={{display:"grid",gridTemplateColumns:"repeat(4,1fr)",gap:"2rem",padding:"2rem"}}>
-    <ShopingCard/>
-    <ShopingCard/>
-    <ShopingCard/>
-    <ShopingCard/>
+    <ShopingCard img={img1} name="Apple iPhone 16 (White, 128 GB)" price={54999} discount={69999} rating={5.4} userbuy={1000}/>
+    <ShopingCard img={img2} name="Samsung Galaxy S23 (Black, 256 GB)" price={74999} discount={89999} rating={4.8} userbuy={800}/>
+    <ShopingCard img={img3} name="OnePlus 11 (Green, 128 GB)" price={56999} discount={64999} rating={4.5} userbuy={600}/>
+    <ShopingCard img={img4} name="Google Pixel 7 (White, 128 GB)" price={59999} discount={64999} rating={4.6} userbuy={500}/>
     </div>
     
    </div>
--- src/components/ShopingCard.jsx
+++ src/components/ShopingCard.jsx
@@ -1,24 +1,24 @@
 import img from "../assets/-original-imah4jyfveygyubc.webp"
-function ShopingCard()
+function ShopingCard(props)
 {
     return(
         <>
-        <div style={{backgroundColor:"lightcyan", width:"300px"}}>
+        <div style={{backgroundColor:"lightcyan", width:"23rem", height:"35rem",borderRadius:"1rem",boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}>
             <div className="main" >
                 <div style={{padding:"1rem"}}>
-                    <img src={img} alt="Iphone" />
+                    <img style={{width:"20rem",height:"20rem"}} src={props.img} alt="Iphone" />
                 </div>
             </div>
             <div >
-                <h1 style={{color:"black",fontSize:"20px",textAlign:"center"}}>Motorola g5</h1>
+                <h1 style={{color:"black",fontSize:"20px",textAlign:"center"}}>{props.name}</h1>
                 <div style={{textAlign:"center",fontSize:"2rem",fontWeight:"bold"}}>
-                    <div style={{display:"inline",color:"black"}}>4.3⭐</div>
-                    <span style={{color:"black"}}>(1200)</span>
+                    <div style={{display:"inline",color:"black"}}>{props.rating}⭐</div>
+                    <span style={{color:"black"}}>({props.userbuy})</span>
                 </div>
                 <div style={{textAlign:"center",fontSize:"1.5rem",fontWeight:"normal"}}>
-                    <div style={{display:"inline",color:"black",marginRight:"1rem" }}>Rs 5000</div>
-                    <div style={{display:"inline",color:"black",marginRight:"1rem"}}>Rs 8000</div>
-                    <div style={{display:"inline",color:"black"}}><span>27%off</span></div>
+                    <div style={{display:"inline",color:"black",marginRight:"1rem" }}>₹ {props.price}</div>
+                    <div style={{display:"inline",color:"black",marginRight:"1rem"}}><s>₹ {props.discount}</s></div>
+                    <div style={{display:"inline",color:"black"}}><span>{Math.floor(props.price/props.discount*100)}% off</span></div>
                     
                 </div>
             </div>
